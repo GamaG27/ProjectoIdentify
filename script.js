8b80a681910cfe45009546f55f89b679f68bc3c9
Damian <Tricasaurio@gmail.com>
2023-07-13 19:13:03 +0200

    Improve styling and refactor code
    - Replaced GIF images with higher-quality SVG images
      - Added 'send_icon.svg' as submit button background
      - Added 'stacked_waves.svg' as background image
      - Replaced 'loader.gif' with 'loader.svg'
    - Implemented loader animation
    - Refactored 'resultSquares' into smaller functions for readability
    - Refactored CSS into logical sections with comments
    - Refactored JavaScript into logical functions
    - Improved styling:
      - Submit button
      - Result display
      - Overall layout
    - Added 'displayName' function to show input name above results
    
    This commit improves the visual styling using higher quality SVG images and a loader animation. It also refactors the code into smaller logical functions for improved readability and maintainability. The CSS and JavaScript have been restructured.

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 const form = document.querySelector('form');
+const centerForm = document.querySelector('.form-container');
 
-// Function to retrieve the API URLs for a given name
-const getApiUrls = (name) => {
-  return [
-    `https://api.nationalize.io?name=${name}`,
-    `https://api.agify.io?name=${name}`,
-    `https://api.genderize.io?name=${name}`,
-  ];
-};
+const getApiUrls = (name) => [
+  `https://api.nationalize.io?name=${name}`,
+  `https://api.agify.io?name=${name}`,
+  `https://api.genderize.io?name=${name}`,
+];
 
 const checkStatus = (response) => {
   if (response.ok) {
@@ -17,78 +15,63 @@ const checkStatus = (response) => {
   }
 };
 
-// Function to round a number to two decimal places and add a percentage sign
-const roundMath = (number) => {
+const roundPercentage = (number) => {
   const decimalPlaces = 2;
   const percentage = (number * 100).toFixed(decimalPlaces);
-  const formattedPercentage =
-    parseFloat(percentage) === parseInt(percentage)
-      ? parseInt(percentage) + '%'
-      : parseFloat(percentage) + '%';
-  return formattedPercentage;
+  return parseFloat(percentage) + '%';
 };
 
 async function fetchData(url) {
-  try {
-    const response = await fetch(url);
-    await checkStatus(response);
-    return await response.json();
-  } catch (error) {
-    console.error(error);
-  }
+  const response = await fetch(url);
+  checkStatus(response);
+  return await response.json();
 }
 
-form.addEventListener('submit', async (event) => {
-  event.preventDefault();
-  const formData = new FormData(event.target);
-  const name = formData.get('name');
+const fetchAllData = async (name) => {
   const apiUrls = getApiUrls(name);
+  return await Promise.all(apiUrls.map(fetchData));
+};
 
-  try {
-    const centerDiv = document.querySelector('.center');
-    centerDiv.innerHTML = '';
+const displayResults = (data) => {
+  displayName(data[0]);
+  displayNationality(data[0]);
+  displayAge(data[1]);
+  displayGender(data[2]);
+};
 
-    const data = await Promise.all(apiUrls.map(fetchData));
-    console.log(data);
+const displayName = (nameData) => {
+  const titleName = document.getElementById('titleName');
+  const resName = nameData.name;
 
-    const actionButtons = document.getElementById('callToAction');
+  titleName.textContent = resName;
+};
 
-    resultSquares(data);
-    insertButtons(actionButtons);
-  } catch (error) {
-    console.error(error);
-
-    let errorMessages = `<p>${error.message}</p>`;
-    showLoadingAnimation(false);
-
-    document.querySelector('.error-messages').innerHTML = errorMessages;
-    showErrorMessage(true);
-  }
-});
-
-const resultSquares = (data) => {
+const displayNationality = (nationData) => {
   const responseNation = document.getElementById('resNation');
-  const countryId = data[0].country[0].country_id;
-  const probabilityNation = data[0].country[0].probability;
+  const countryId = nationData.country[0].country_id;
+  const probabilityNation = nationData.country[0].probability;
 
   responseNation.innerHTML = `
-    <p>There is a <b>${roundMath(
+    <p>There is a <b>${roundPercentage(
       probabilityNation
-    )}</b> probability that you are fro ${countryId} <br></p>
-    <img src="https://flagsapi.com/${countryId}/flat/64.png" alt="The flag of ${countryId}">
-  `;
+    )}</b> probability that you are from ${countryId}</p>
+    <img src="https://flagsapi.com/${countryId}/flat/64.png" alt="The flag of ${countryId}">`;
+};
 
+const displayAge = (ageData) => {
   const responseAge = document.getElementById('resAge');
-  const age = data[1].age;
+  const age = ageData.age;
 
   responseAge.innerHTML = `<p><b>${age}</b> years old</p>`;
+};
 
+const displayGender = (genderData) => {
   const responseGender = document.getElementById('resGender');
-  const gender = data[2].gender;
-  const probabilityGender = data[2].probability;
+  const gender = genderData.gender;
+  const probabilityGender = genderData.probability;
 
   responseGender.innerHTML = `
-    <p>There is a <b>${roundMath(
+    <p>There is a <b>${roundPercentage(
       probabilityGender
     )}</b> probability that you are a biological <b>${gender}</b></p>
   `;
@@ -96,12 +79,13 @@ const resultSquares = (data) => {
 
 const showLoadingAnimation = (isLoading) => {
   const loadingScreen = document.querySelector('.loading');
+  const resultDiv = document.querySelector('.result');
   if (isLoading) {
     loadingScreen.classList.remove('hidden');
-    stageContainer.classList.add('hidden');
+    centerForm.classList.add('hidden');
   } else {
     loadingScreen.classList.add('hidden');
-    stageContainer.classList.remove('hidden');
+    resultDiv.classList.remove('hidden');
   }
 };
 
@@ -109,6 +93,7 @@ const showErrorMessage = (isError) => {
   const loadingScreen = document.querySelector('.error');
   if (isError) {
     loadingScreen.classList.remove('hidden');
+    centerForm.classList.add('hidden');
   } else {
     loadingScreen.classList.add('hidden');
   }
@@ -148,7 +133,7 @@ const insertButtons = (div) => {
 
     // Create a "Try Again" button
     const tryAgainButton = document.createElement('button');
-    tryAgainButton.textContent = 'Try Again';
+    tryAgainButton.textContent = 'Try Another Name';
 
     // Add event listener to the "Try Again" button
     tryAgainButton.addEventListener('click', () => {
@@ -189,3 +174,29 @@ const insertButtons = (div) => {
     div.appendChild(tryAgainButton);
   });
 };
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const name = formData.get('name');
+
+  try {
+    showLoadingAnimation(true);
+    const data = await fetchAllData(name);
+    console.log(data);
+
+    setTimeout(() => {
+      showLoadingAnimation(false);
+      displayResults(data);
+      insertButtons(document.getElementById('callToAction'));
+    }, 5000);
+  } catch (error) {
+    console.error(error);
+
+    showLoadingAnimation(false);
+    document.querySelector(
+      '.error-messages'
+    ).innerHTML = `<p>${error.message}</p>`;
+    showErrorMessage(true);
+  }
+});
